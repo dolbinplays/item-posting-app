f259a3dad6f6c5137c596ed949f654087dbd25c4
dolbinplays <dolbin.plays@gmail.com>
2026-05-31 21:24:39 -0700

    Move PWA install control into setup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'item-posting-assistant-shell-v0.26.06.01.0100';
+const CACHE_NAME = 'item-posting-assistant-shell-v0.26.06.01.0110';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', event => {
