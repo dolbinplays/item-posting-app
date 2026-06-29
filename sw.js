2b94eaac8c940741b8100e03275d0c0409c1d513
dolbinplays <dolbin.plays@gmail.com>
2026-06-28 20:08:36 -0700

    Add printable sold items report

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'item-posting-assistant-shell-v0.26.06.01.0510';
+const CACHE_NAME = 'item-posting-assistant-shell-v0.26.06.01.0520';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/app-icon.svg', './icons/app-icon-192.png', './icons/app-icon-512.png'];
 
 self.addEventListener('install', event => {
